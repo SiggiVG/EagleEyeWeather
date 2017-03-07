6403e9688d26e8d8ff204438707fccb954d52cd5
VikingGoth <wolfdude131@gmail.com>
2017-03-07 13:16:23 -0500

    removed a redundant class???

---
+++ app/src/main/java/edu/friedrich/kaiser/weatherapp/weather/WeeklyWeather.java
@@ -0,0 +1,23 @@
+package edu.friedrich.kaiser.weatherapp.weather;
+
+import org.json.JSONException;
+
+/**
+ * Created by Friedrich on 2/27/2017.
+ */
+
+public class WeeklyWeather extends Weather
+{
+    public WeeklyWeather(String str) throws JSONException {
+        super(str);
+        this.parseJson();
+    }
+
+    @Override
+    public Weather parseJson()
+    {
+        //TODO: parse the JSON file, putting each value within to a variable above.
+
+        return this;
+    }
+}
